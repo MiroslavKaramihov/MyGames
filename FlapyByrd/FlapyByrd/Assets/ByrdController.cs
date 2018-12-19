883eb05ebe9fe24d2a6481768b49456d9aecf6d7
Miroslav Kostov Karamihov <45258645+MiroslavKaramihov@users.noreply.github.com>
2018-12-19 12:21:06 +0200

    MyGames

---
+++ FlapyByrd/FlapyByrd/Assets/ByrdController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class ByrdController : MonoBehaviour
+{
+    private Rigidbody2D rigidbody;
+
+    public void Start()
+    {
+        this.rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+
+    public void Update()
+    {
+
+    }
+}
